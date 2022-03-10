d3f56078cba5cecbe0392f1f219d3886462f12f1
Stepan Urivko <85336783+ssurivko20@users.noreply.github.com>
2022-03-10 22:59:41 +0200

    main-page

--- website/JavaScript/script.js
+++ website/JavaScript/script.js
@@ -1,6 +1,39 @@
-
-function changeBackground (){
-    document.getElementById("header-image").style.backgroundImage = "url('https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80')";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-// setTimeout(changeBackground(), 2000);
+function setBackgroundImage(){
+  var imgSources = ["images/head-back-ancient.jpg", "images/head-back-medie.jpg",
+  "images/head-back-18th.jpg", "images/head-back-20th.jpg"];
+
+  var quotes = ['"Only the dead have seen the end of war."<br>— Plato', '"God created war so that Americans would learn geography."<br>― Mark Twain',
+  '"War is what happens when language fails."<br>― Margaret Atwood', '"I know not with what weapons World War III will be fought,<br> but World War IV will be fought with sticks and stones."<br>― Albert Einstein'];
+  var qlen = quotes.length;
+  document.getElementById("quote").innerHTML = quotes[getRandomInt(qlen)];
+
+  var len = imgSources.length;
+  var randint = getRandomInt(len);
+  var image = new Image();
+  image.src = imgSources[randint];
+  $("#header-image").css("background-image", "url('" + image.src + "')");
+  if (randint == 0) {
+    document.getElementById("heading").style.fontFamily = 'greek';
+    document.getElementById("heading").innerHTML = 'HISTORY OF WARS';
+  }
+  else if (randint == 1) {
+    document.getElementById("heading").style.fontFamily = 'mediavel';
+    document.getElementById("heading").innerHTML = 'History of wars';
+  }
+  else if (randint == 2) {
+    document.getElementById("heading").style.fontFamily = 'enlight';
+    document.getElementById("heading").innerHTML = 'History of wars';
+  }
+  else if (randint == 3) {
+    document.getElementById("heading").style.fontFamily = 'modern';
+    document.getElementById("heading").innerHTML = 'HISTORY OF WARS';
+  }
+}
+
+setBackgroundImage();
+
+var timeid = setInterval(function() {setBackgroundImage();}, 10000);

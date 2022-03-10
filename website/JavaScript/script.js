289208140d289ec06d616c6826860c4dad32123d
Stepan Urivko <85336783+ssurivko20@users.noreply.github.com>
2022-03-10 02:00:48 +0200

    main-page

---
+++ website/JavaScript/script.js
@@ -0,0 +1,6 @@
+
+function changeBackground (){
+    document.getElementById("header-image").style.backgroundImage = "url('https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80')";
+}
+
+// setTimeout(changeBackground(), 2000);
